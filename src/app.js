d4207fba7d89e7575108c54cf96fa840f4ede433
Jonathan vera <167922770+jonathanavl@users.noreply.github.com>
2024-06-14 10:04:34 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let numcard = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let randomnum = Math.floor(Math.random() * numcard.length);
+  let randomCard = numcard[randomnum];
+  document.getElementById("card").innerText = randomCard;
+
+  let palo = ["♦", "♥", "♠", "♣"];
+  let randompalo = Math.floor(Math.random() * palo.length);
+  let randomspalos = palo[randompalo];
+  document.getElementById("palos").innerText = randomspalos;
+  document.getElementById("palos2").innerText = randomspalos;
+
+  if (randomspalos === "♦" || randomspalos === "♥") {
+    document.getElementById("palos").classList.add("red");
+    document.getElementById("palos2").classList.add("red");
+  } else {
+    document.getElementById("palos").classList.add("black");
+    document.getElementById("palos2").classList.add("black");
+  }
 };
